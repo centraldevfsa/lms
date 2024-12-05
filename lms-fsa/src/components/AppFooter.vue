13f0019a86ab03c8325c3c10423f800abf4bdc65
Alessandro <alespbecker@gmail.com>
2024-12-04 23:42:50 -0300

    blah

--- lms-fsa/src/components/AppFooter.vue
+++ lms-fsa/src/components/AppFooter.vue
@@ -2,7 +2,7 @@
     <footer class="text-white py-6">
       <div class="container mx-auto px-4 pt-24 border border-black">
         <div class="flex flex-col items-center">
-          <p class="text-sm">© 2024 FSA - Faculdade de Saúde Avançada. Todos os direitos reservados.</p>
+          <p class="text-lg">© 2024 FSA - Faculdade de Saúde Avançada. Todos os direitos reservados.</p>
           <div class="flex space-x-4 mt-2">
             <a href="#" class="hover:underline">Política de Privacidade</a>
             <a href="#" class="hover:underline">Termos de Serviço</a>
