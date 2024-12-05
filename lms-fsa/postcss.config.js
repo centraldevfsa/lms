fa07cf9be60299cf7e0dc30f1b8e32e0be56ae3e
Alessandro <alespbecker@gmail.com>
2024-12-04 22:21:36 -0300

    Mais um commit 041224 22:22

--- lms-fsa/postcss.config.js
+++ lms-fsa/postcss.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
+    postcssOptions: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
     },
   };
   
