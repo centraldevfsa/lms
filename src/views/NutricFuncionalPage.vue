e7d0972e7d70b05c4cdd953a40e5a8110e1c0fb9
Alessandro <alespbecker@gmail.com>
2024-12-15 05:02:24 -0300

    l

--- src/views/NutricFuncionalPage.vue
+++ src/views/NutricFuncionalPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Nutrição Funcional</h1>
+    <h1 class="text-white">Nutrição Funcional</h1>
     <p>Página dedicada à formação de Nutrição Funcional.</p>
   </div>
 </template>
